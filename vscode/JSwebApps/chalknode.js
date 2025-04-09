ee11e2d9e68475fccbbc647a45e706db7fc23899
kaushal19097 <ckaushal56@gmail.com>
2025-04-09 22:47:52 +0530

    .

--- vscode/JSwebApps/chalknode.js
+++
@@ -1,4 +0,0 @@
-let chalk = require('chalk');
-console.log(chalk.blue.bold("Hello, Node.js!"));
-console.log(chalk.yellow("Caution: Low Disk Space"));
-console.log(chalk.green("Installation Complete!"));
--- vscode/JSwebApps/nodefile.js
+++
@@ -1,4 +0,0 @@
-let moment=require('moment');
-console.log("Welcome to Node.js!");
-const currentDateTime = moment().format('YYYY-MM-DD HH:mm:ss');
-console.log(`Current Date and Time: ${currentDateTime}`);
